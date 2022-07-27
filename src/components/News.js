1607d7c52260af2bf8bc60060ae271661e91d06a
shyamsutar <shyamsutar629@gmail.com>
2022-07-27 22:09:30 +0530

    applied news api

--- src/components/News.js
+++ src/components/News.js
@@ -2,54 +2,45 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export default class News extends Component {
+
+ 
+  constructor(){
+    super();
+    console.log("Hello I am a constructor from News component"); 
+    this.state = {
+      articles : [],
+      loading : false
+
+    }    
+  }
+
+  async componentDidMount(){
+    console.log("cdm");
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=df3b31e044564987b6855d6d5654757c";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles});
+  }
+
   render() {
     return (
       <>
+        <div className="container my-8 mx-auto">
         <h1 className="text-3xl font-bold">CodeMonkey - Top Headlines</h1>
-        <div className="container my-3">
-          <div class="grid grid-cols-3 grid-flow-row gap-4">
-            <div>
-              <Newsitem
-                title="MyTitle"
-                description="losdfaj sdfj sadjf asdjf asdfj sadfjs dfj"
-              />
-            </div>
-           <div>
-              <Newsitem
-                title="MyTitle"
-                description="losdfaj sdfj sadjf asdjf asdfj sadfjs dfj"
-              />
-            </div>
-           <div>
-              <Newsitem
-                title="MyTitle"
-                description="losdfaj sdfj sadjf asdjf asdfj sadfjs dfj"
-              />
-            </div>
-           <div>
-              <Newsitem
-                title="MyTitle"
-                description="losdfaj sdfj sadjf asdjf asdfj sadfjs dfj"
-              />
-            </div>
-           <div>
-              <Newsitem
-                title="MyTitle"
-                description="losdfaj sdfj sadjf asdjf asdfj sadfjs dfj"
-              />
-            </div>
-           <div>
-              <Newsitem
-                title="MyTitle"
-                description="losdfaj sdfj sadjf asdjf asdfj sadfjs dfj"
-              />
-            </div>
-           <div>
-              <Newsitem
-                title="MyTitle"
-                description="losdfaj sdfj sadjf asdjf asdfj sadfjs dfj"
-              />
-            </div>
+          <div className="grid grid-cols-3 grid-flow-row gap-4 my-8">
+            
+        {this.state.articles.map((element)=>{
+          return <Newsitem
+                key={element.url}
+                title={element.title?element.title:""}
+                description={element.description?element.description:""}
+                imageUrl = {element.urlToImage}
+                newsUrl = {element.url}
+/>
+        })}
+
+              
            
           </div>
         </div>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
+
+
+
   render() {
 
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
 
     return (
       <div>
 
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
+            <img className="w-full" src={!imageUrl?"https://images.wsj.net/im-591376/social":imageUrl} alt="Sunset in the mountains"/>
             <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{title}</div>
             <p className="text-gray-700 text-base">
-                {description}
+                {description}...
             </p>
             </div>
             <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            <a  href={newsUrl} className='text-blue-500'>Read more...</a>
             </div>
             </div>
 
